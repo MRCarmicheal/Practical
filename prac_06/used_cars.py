6df13c24130ae21b80053a74c9d1f3a1e1a00b4e
Wang qi <qi.wang5@my.jcu.edu.au>
2022-04-29 22:24:03 +0800

    prac_06

---
+++ prac_06/used_cars.py
@@ -0,0 +1,18 @@
+from car import Car
+
+
+def main():
+    my_car = Car("Car", 42)
+    my_car.drive(0)
+    print("fuel =", my_car.fuel)
+    print("odo =", my_car.odometer)
+    print(my_car)
+
+    print("Car {}, {}".format(my_car.fuel, my_car.odometer))
+    print("Car {self.fuel}, {self.odometer}".format(self=my_car))
+
+    limo = Car("Limo", 100)
+    limo.add_fuel(20)
+    print(limo.fuel)
+    limo.drive(115)
+    print(limo.odometer)
